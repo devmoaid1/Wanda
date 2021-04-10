97409e2c7600399b8977f13f6049e8ff6f43e76e
devmoaid1 <68110578+devmoaid1@users.noreply.github.com>
2021-04-10 21:42:45 +0800

    added user auth to customer model

--- backend/mainapp/models.py
+++ backend/mainapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -38,6 +38,8 @@ class Customer(models.Model):
     phone = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     bookings = models.ManyToManyField(Booking)
+    owner = models.ForeignKey(
+        User, related_name="customer", on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
