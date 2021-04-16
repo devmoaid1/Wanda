cc293c4d00598c74773a6b8e86605f1a11e4a36d
devmoaid1 <68110578+devmoaid1@users.noreply.github.com>
2021-04-17 02:16:34 +0800

    modified login and signup forms

--- frontend/src/Views/Login/login.js
+++ frontend/src/Views/Login/login.js
@@ -1,9 +1,9 @@
 import React,{Component,Fragment} from 'react'; 
-import{Container,Form ,Col,Row,Button,Spinner} from 'react-bootstrap'; 
+import{Spinner} from 'react-bootstrap'; 
 //import logo from '../images/wanda.svg'
 import logphoto from '../../images/loginphoto.jpeg' 
 import {connect} from 'react-redux'
-import {NavLink} from 'react-router-dom'
+
 import './login.css'
 
 class LoginForm extends Component{
@@ -27,58 +27,47 @@ class LoginForm extends Component{
             
                  <Spinner animation="border" variant="danger" />:
             
-            <div>  
+                 <div className="w-full h-full  ">  
                 
-                <div className="mb-10 mt-4 h-full">
-
-
-
-
-             <Container id="LoginContainer" >
-               <Row noGutters={true}>
-                   <Col lg={5}> <img src={logphoto} className="img-fluid" alt="logo"></img></Col>
-
-                   <Col className="lg-7 px-5 pt-5">
-                    <h1 className=" text-5xl font-bold mb-3">Wanda</h1>
-                    <h4 >Sign in into your account</h4>
-                   <Form>
-            
-<Form.Group className="form-row" id="formHorizontalEmail">
-    <Col lg={7} >
-      <Form.Control type="email" placeholder="Email" className="form-control my-3 p-4" />
-    </Col>
-</Form.Group>
-
-<Form.Group className="form-row" id="formHorizontalPassword">
-    <Col lg={7}>
-      <Form.Control type="password" placeholder="Password" className="form-control my-3 p-4" />
-    </Col>
- </Form.Group>
-
-<Form.Group className="form-row">
-    <Col lg={7}>
-      <Button type="submit"  className="btn1">Login</Button>
-    </Col>
-</Form.Group>
-<Form.Group className="form-row">
-    <Col lg={7}>
-    <span>Don`t Have an account? </span><NavLink to="/signup/"> <span className="signup">SignUp</span></NavLink>
-    </Col>
-</Form.Group>
+                 <div className="mb-10 mt-4 rounded-md overflow-hidden shadow-lg w-2/3   mx-auto flex flex row">
+ 
+                 <div className="h-full"><img src={logphoto} alt="logphoto"></img></div> 
+                 <div className=" mt-5 mb-3  pl-3">
+                     <h1 className="text-5xl text-bold ml-4 mt-6 ">Login</h1> 
+                     <h1 className='ml-4 mt-6'> Sign in with your  account </h1>
+                     
+                     <form className="w-full mt-6 "> 
+                                            
+  
+     <div class="mb-2 ml-4 mt-1 mr-2">
+       <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+         Username
+       </label>
+       <input class="border border-transparent focus:outline-none focus:ring-2 focus:ring-pink-400 focus:border-transparent rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:shadow-outline" id="username" type="text" placeholder="username"/>
+       
+     </div>
+     <div class="mb-2 ml-4 mt-2 mr-2">
+       <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+         Password
+       </label>
+       <input class="border border-transparent focus:outline-none focus:ring-2 focus:ring-pink-400 focus:border-transparent rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:shadow-outline" id="password" type="password" placeholder="*********************"/>
+       
+     </div>
      
-</Form>
-</Col>
-                   
-                                 
-
-</Row>
-               
-
-
-             </Container> 
-             </div>
-                
+     <button type="submit"class="bg-red-500 hover:bg-red-400 text-white  py-2 px-4 ml-4 mx-2 rounded">
+   Login
+ </button>
+   <span className="block ml-4 mt-2">Dont have an account? <span className="text-red-500 hover:text-red-700"><a href="/signup/">Sign Up </a></span></span>
+  
+                     </form>
+ 
                  </div>
+ 
+ 
+             
+              </div>
+                 
+                  </div>
             
     }
 
--- frontend/src/Views/Sign up/signup.js
+++ frontend/src/Views/Sign up/signup.js
@@ -1,13 +1,28 @@
 import React,{Component,Fragment} from 'react'; 
-import{Container,Form ,Col,Row,Button,Spinner} from 'react-bootstrap'; 
+import{Spinner} from 'react-bootstrap'; 
 //import logo from '../images/wanda.svg'
 import logphoto from '../../images/loginphoto.jpeg' 
 import {connect} from 'react-redux'
-import {NavLink} from 'react-router-dom'
+
 import '../Login/login.css'
 
 class SignUpForm extends Component{
     
+  
+
+   alertMassage=(title)=>{
+     return(
+      <div role="alert">
+      <div class="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+        Danger
+      </div>
+      <div class="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
+        <p>{title}</p>
+      </div>
+    </div>
+     )
+   }
+
   
 
     render(){
@@ -27,67 +42,73 @@ class SignUpForm extends Component{
             
                  <Spinner animation="border" variant="danger" />:
             
-            <div>  
+            <div className="w-full h-full ">  
                 
-                <div className="mb-10 mt-4 h-full w-full">
+                <div className="mb-10 mt-4 rounded-md overflow-hidden shadow-lg w-2/3 mx-auto flex flex row">
+
+                <img src={logphoto} alt="logphoto"></img> 
+                <div className=" mb-3  py-3">
+                    <h1 className="text-5xl text-bold ml-4 mt-3">Sign Up</h1> 
+                    <h1 className='ml-4 mt-3'> Sign up with new account </h1>
+                    
+                    <form className="w-full"> 
+                      <div className="flex flex wrap ml-3  "> 
+                      <div class=" ml-2 mt-3 ">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+        Username
+      </label>
+      <input class="border border-transparent focus:outline-none focus:ring-2 focus:ring-red-400 focus:border-transparent rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:shadow-outline" id="username" type="text" placeholder="Username" name="username"/>
+    </div>
+    <div class="mb-2 ml-2 mt-3">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Password
+      </label>
+      <input class="border border-transparent focus:outline-none focus:ring-2 focus:ring-red-400 focus:border-transparent rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:shadow-outline" id="password" type="password" placeholder="****************" name="password"/>
+      
+    </div>
+                       
+                      </div>
+                    
+ 
+    <div class="mb-2 ml-4 mt-1 mr-2">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="fullname">
+        Full Name
+      </label>
+      <input class="border border-transparent focus:outline-none focus:ring-2 focus:ring-red-400 focus:border-transparent rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:shadow-outline" id="fullname" type="text" placeholder="Full Name" name="fullname"/>
+      
+    </div>
+    <div class="mb-2 ml-4 mt-2 mr-2">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="emailaddress">
+        Email Address
+      </label>
+      <input class="border border-transparent focus:outline-none focus:ring-2 focus:ring-red-400 focus:border-transparent rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:shadow-outline" id="emailaddress" type="password" placeholder="Email Address" name="emailaddress"/>
+      
+    </div>
+     <div class="mb-2 ml-4 mt-2 mr-2">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="Phone Number">
+        Phone Number
+      </label>
+      <input class="border border-transparent focus:outline-none focus:ring-2 focus:ring-red-400 focus:border-transparent rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:shadow-outline" id="phonenumber" type="text" placeholder=" phone number"  name="phonenumber"/>
+      
+    </div>
+    <div class="mb-2 ml-4 mt-2 mr-2">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="address">
+       Address
+      </label>
+      <input class="border border-transparent focus:outline-none focus:ring-2 focus:ring-red-400 focus:border-transparent rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:shadow-outline" id="address" type="text" placeholder="Address" name="address"/>
+      
+    </div>
+    <button type="submit"class="bg-red-500 hover:bg-red-600 text-white  py-2 px-4 ml-4 mx-2 rounded">
+  Sign Up
+</button>
+ 
+ 
+                    </form>
+
+                </div>
 
 
-
-
-             <Container id="LoginContainer" >
-               <Row noGutters={true}>
-                   <Col lg={5}> <img src={logphoto} className="img-fluid" alt="logo"></img></Col>
-
-                   <Col className="lg-8 px-5 pt-5">
-                    <h1 className=" text-5xl font-bold mb-3">Sign Up</h1>
-                    <h4 >Sign Up with a new Account</h4>
-                   <Form>
             
-<Form.Group className="form-row" id="formHorizontalEmail">
-    <Col lg={7} >
-      <Form.Control type="email" placeholder="Email" className="form-control my-2 p-4" />
-    </Col>
-</Form.Group>
-<Form.Group className="form-row" id="formHorizontalEmail">
-    <Col lg={7} >
-      <Form.Control type="password" placeholder="password" className="form-control my-2 p-4" />
-    </Col>
-</Form.Group>
-<Form.Group className="form-row" id="formHorizontalEmail">
-    <Col lg={7} >
-      <Form.Control type="text" placeholder="FullName" className="form-control my-2 p-4" />
-    </Col>
-</Form.Group>
-
-<Form.Group className="form-row" id="formHorizontalPassword">
-    <Col lg={7}>
-      <Form.Control type="text" placeholder="Phone Number" className="form-control my-2 p-4" />
-    </Col>
- </Form.Group>
- <Form.Group className="form-row" id="formHorizontalPassword">
-    <Col lg={7}>
-      <Form.Control type="text" placeholder="Address" className="form-control my-2 p-4" />
-    </Col>
- </Form.Group> 
-
-
-<Form.Group className="form-row">
-    <Col lg={7}>
-      <Button type="submit"  className="btn1">Sign UP</Button>
-    </Col>
-</Form.Group>
-
-     
-</Form>
-</Col>
-                   
-                                 
-
-</Row>
-               
-
-
-             </Container> 
              </div>
                 
                  </div>
