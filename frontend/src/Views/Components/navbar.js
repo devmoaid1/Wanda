5d11abe076306c0c3ce55e76110616803df22873
devmoaid1 <68110578+devmoaid1@users.noreply.github.com>
2021-04-19 03:10:54 +0800

    update navbar.js

--- frontend/src/Views/Components/navbar.js
+++ frontend/src/Views/Components/navbar.js
@@ -1,6 +1,8 @@
-import React from 'react'; 
+import React,{Component} from 'react'; 
 import logo from'../../images/wandalogo.svg'
 import './navbar.css'
+import {connect} from 'react-redux'
+import * as actions from '../../actions/auth'
 
 //import {NavLink} from 'react-router-dom';
 // import {
@@ -13,27 +15,44 @@ import './navbar.css'
 // } from 'react-bootstrap';
 
 
-const NavBarComponent = () => {
-  return (
-    <div style={{background:'#091c29'}}>
-      <div className="flex items-center justify-between w-10/12 mx-auto py-1 text-white"> 
-        <div className="flex flex-row justify-left pt-3  text-white"><img width="50"
-        height="50" src={logo} alt="logo"></img>        
-        
-         <h1 className="text-2xl font-sans font-bold text-white"> Wanda</h1>
+class NavBarComponent extends Component {
+  
+  
+  render(){
+    return (
+      <div style={{background:'#091c29'}}>
+        <div className="flex items-center justify-between w-10/12 mx-auto py-1 text-white"> 
+          <div className="flex flex-row justify-left pt-3  text-white"><img width="50"
+          height="50" src={logo} alt="logo"></img>        
+          
+           <h1 className="text-2xl font-sans font-bold text-white"> Wanda</h1>
+          </div>
+           <div className="inline-block">
+             <span className="text-xl mr-4  focus:ring-white"><a className=" hover:purple-700" href="/home/">Home</a></span>
+             <span  className="text-xl mr-4"><a href="/cars/">Cars</a></span>
+             { this.props.isAuthenticated?
+             <span  className="text-xl mr-4"><a href="/login/" onClick={this.props.logout}>Logout</a></span>:
+             <span  className="text-xl mr-4"><a href="/login/">Login</a></span> 
+             }
+           </div>
+  
         </div>
-         <div className="inline-block">
-           <span className="text-xl mr-4  focus:ring-white"><a className=" hover:purple-700" href="/home/">Home</a></span>
-           <span  className="text-xl mr-4"><a href="/cars/">Cars</a></span>
-           <span  className="text-xl mr-4"><a href="/login/">Login</a></span>
-         </div>
-
       </div>
-    </div>
-  )
+    )
+ 
+  }
+  
 }
 
-export default NavBarComponent;
+
+
+const mapDispatchToProps = dispatch => {
+return {
+    logout: () => dispatch(actions.logout()) 
+}
+}
+
+export default connect(null,mapDispatchToProps)(NavBarComponent);
 
 /*
  <Navbar bg="dark" variant="dark">
