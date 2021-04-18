45cc9031ee419fdd74ef80dbb0fed50418c2ed92
devmoaid1 <68110578+devmoaid1@users.noreply.github.com>
2021-04-18 22:34:25 +0800

    created car detail file

---
+++ frontend/src/Views/Cars/car_detail.js
@@ -0,0 +1,16 @@
+import React ,{ Component } from "react"
+
+
+class CarDetail extends Component{
+
+
+    render(){
+        return(
+
+            <div></div>
+        )
+    }
+}
+
+
+export default CarDetail
