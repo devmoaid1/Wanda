693cfda0d714ef40b83d4ed300a4521646664df4
devmoaid1 <68110578+devmoaid1@users.noreply.github.com>
2021-05-19 08:24:58 +0800

    edited booking status layout

--- frontend/src/Views/Admin/Bookings/ManageBookings.js
+++ frontend/src/Views/Admin/Bookings/ManageBookings.js
@@ -70,7 +70,10 @@ function ManageBookings() {
             </td>
             <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
                 <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Status</span>
-                <span class="rounded bg-yellow-400 py-1 px-3 text-xs font-bold">{booking.status}</span>
+                {booking.status==="pending"?
+                <span class="rounded bg-yellow-500 py-1 px-3 text-xs text-white font-semibold">{booking.status}</span>:
+                <span class="rounded bg-green-500 py-1 px-3 text-xs text-white font-semibold">{booking.status}</span> 
+            }
             </td>
             <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
                 <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Actions</span>
